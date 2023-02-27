084fe7b6a7c362ac63332a1d8b1bb634d5ce25c9
bob colin <bobbcolin@gmail.com>
2023-02-27 13:21:04 +0100

    before expo upgrade

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Image } from 'react-native';
 import {Dimensions} from 'react-native';
+import { useState } from 'react';
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 
 export default function LoginScreen() {
+    const [showSignUp,setShowSignUp] = useState(false);
+
   return (
     
     <View style={styles.container}>
         <View style={styles.window}>
-            <Image />
+            <Image 
+                style={styles.tinyLogo}
+                source={require('../assets/TR-logo-200x200.png')}/>
+            <Text style={styles.pageTitle}> CONNEXION</Text>
+            <Text > Pas encore de compte ?</Text>
+            <Text style={styles.linkSignUp} onPress={()=>setShowSignUp(true)}>Cliquez pour vous inscrire</Text>
+            <View style={styles.sepContainer}>
+                <View style={styles.sepLine} />
+                <View>
+                    <Text style={styles.sepText}>Me connecter avec mon email</Text>
+                </View>
+                <View style={styles.sepLine} />
+            </View>
         </View>
     </View>
 
@@ -29,4 +44,27 @@ const styles = StyleSheet.create({
     justifyContent:'flex-start',
     alignItems:'center',
   },
+  tinyLogo:{
+    width:100,
+    height:100,
+  },
+  pageTitle:{
+
+  },
+  linkSignUp:{
+
+  },
+  sepContainer:{
+    flexDirection: 'row', 
+    alignItems: 'center'
+  },
+  sepLine:{
+    flex: 1, 
+    height: 1, 
+    backgroundColor: 'black',
+  },
+  sepText:{
+    width: 50, 
+    textAlign: 'center',
+  }
 });
