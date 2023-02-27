ecf6c8b7f803f45e54d8b0c2383a86bd355e51b5
bob colin <bobbcolin@gmail.com>
2023-02-27 15:21:22 +0100

    main propre

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
+// IMPORT --------------------------------------------------------------------
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Link } from 'react-native';
+
+// STORE -------------------------------------------------------------------
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import user from './reducers/user';
+const store = configureStore({
+  reducer: {user},
+ });
+
+// NAVIGATION --------------------------------------------------------------
+import LoginScreen from './screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return (
+  <Provider store={store}>
+    <NavigationContainer>
+    <Stack.Navigator screenOptions={{ headerShown: true }}>
+     <Stack.Screen name="Login" component={LoginScreen} />
+   </Stack.Navigator>
+  </NavigationContainer>
+  </Provider>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
 });
