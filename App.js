65c2bcdf0ce4242c035eb045990fa649e6f3511c
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-02-28 09:20:54 +0100

    drawer a terminer

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import * as React from "react";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { Animated, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import profile from './assets/profile.png';
 
 // Screens
 import MaJourneeScreen from "./screens/MaJourneeScreen";
 import MonProfilScreen from "./screens/MonProfilScreen";
 import LoginScreen from "./screens/LoginScreen";
 import ItineraireScreen from "./screens/ItineraireScreen";
+
+// Tab Icons
+import home from './assets/home.png';
+import search from './assets/search.png';
+import notifications from './assets/bell.png';
+import settings from './assets/settings.png';
+import logout from './assets/logout.png'
+
+// Menu
+import menu from './assets/menu.png';
+import close from './assets/close.png';
+
+// Photo
+import photo from './assets/photo.jpg';
 
 // Navigation
 import { NavigationContainer } from "@react-navigation/native";
@@ -44,11 +61,11 @@ const DrawerNavigator = () => {
                 },
                 tabBarActiveTintColor: "#ec6e5b",
                 tabBarInactiveTintColor: "#335561",
-                headerShown: true,
+                headerShown: false,
             })}
-            drawerPosition="right"
         >
             <Drawer.Screen name="maJournee" component={MaJourneeScreen} />
+            <Drawer.Screen name="login" component={Login} />
             <Drawer.Screen name="user" component={MonProfilScreen} />
             <Drawer.Screen name="itineraire" component={ItineraireScreen} />
         </Drawer.Navigator>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,70 +1,131 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image } from 'react-native';
-import {Dimensions} from 'react-native';
-import { useState } from 'react';
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-export default function LoginScreen() {
-    const [showSignUp,setShowSignUp] = useState(false);
-
-  return (
-    
-    <View style={styles.container}>
-        <View style={styles.window}>
-            <Image 
-                style={styles.tinyLogo}
-                source={require('../assets/Mini-logo.png')}/>
-            <Text style={styles.pageTitle}> CONNEXION</Text>
-            <Text > Pas encore de compte ?</Text>
-            <Text style={styles.linkSignUp} onPress={()=>setShowSignUp(true)}>Cliquez pour vous inscrire</Text>
-            <View style={styles.sepContainer}>
-                <View style={styles.sepLine} />
-                <View>
-                    <Text style={styles.sepText}>Me connecter avec mon email</Text>
+export default function MaJourneeScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            {/* Profil */}
+            <View style={styles.profilContainer}>
+                <Image source={require("../assets/demoAvatar.png")} style={styles.avatar} />
+                <View style={styles.info}>
+                    <Text style={styles.prenom}>Prénom</Text>
+                    <Text style={styles.nom}>Nom</Text>
                 </View>
-                <View style={styles.sepLine} />
+            </View>
+
+            <View style={styles.boxIncluedTitle}>
+                <Text style={styles.statsTitle}>Statistiques Semaine</Text>
+                {/* Rdv pris */}
+                <View style={styles.allItems}>
+                    <View style={styles.stats}>
+                        <View style={styles.iconGroup}>
+                            <FontAwesome name="calendar-o" size={30} color="#000" style={styles.Icon} />
+                            <Text style={styles.statsData}>100%</Text>
+                        </View>
+                        <Text style={styles.statsCount}>18</Text>
+                        <Text style={styles.statsRdv}>rdv terminés</Text>
+                    </View>
+
+                    {/* Rdv validés */}
+                    <View style={styles.stats}>
+                        <View style={styles.iconGroup}>
+                            <FontAwesome name="check" size={30} color="#000" style={styles.Icon} />
+                            <Text style={styles.statsData}>100%</Text>
+                        </View>
+                        <Text style={styles.statsCount}>9</Text>
+                        <Text style={styles.statsRdv}>rdv terminés</Text>
+                    </View>
+
+                    {/* Rdv convertis */}
+                    <View style={styles.stats}>
+                        <View style={styles.iconGroup}>
+                            <FontAwesome name="calendar-o" size={30} color="#000" style={styles.Icon} />
+                            <Text style={styles.statsData}>100%</Text>
+                        </View>
+                        <Text style={styles.statsCount}>6</Text>
+                        <Text style={styles.statsRdv}>rdv terminés</Text>
+                    </View>
+
+                    {/* Tps moyen */}
+                    <View style={styles.stats}>
+                        <View style={styles.iconGroup}>
+                            <FontAwesome name="clock-o" size={30} color="#000" style={styles.Icon} />
+                            <Text style={styles.statsData}>38 min</Text>
+                        </View>
+                        <Text style={styles.statsRdv}>temps moyen / rdv</Text>
+                    </View>
+                </View>
             </View>
         </View>
-    </View>
-
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  window:{
-    height:screenHeight,
-    width:screenWidth,
-    justifyContent:'flex-start',
-    alignItems:'center',
-  },
-  tinyLogo:{
-    width:100,
-    height:100,
-  },
-  pageTitle:{
-
-  },
-  linkSignUp:{
-
-  },
-  sepContainer:{
-    flexDirection: 'row', 
-    alignItems: 'center'
-  },
-  sepLine:{
-    flex: 1, 
-    height: 1, 
-    backgroundColor: 'black',
-  },
-  sepText:{
-    textAlign: 'center',
-    marginHorizontal:10,
-  }
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "flex-start",
+    },
+    profilContainer: {
+        borderColor: "green",
+        borderWidth: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    avatar: {
+        width: 100,
+        height: 100,
+    },
+    info: {
+        borderColor: "red",
+        borderWidth: 1,
+        marginLeft: 20,
+    },
+    prenom: {
+        fontSize: 20,
+        marginBottom: 10,
+    },
+    nom: {
+        fontSize: 20,
+    },
+    boxIncluedTitle: {
+        borderColor: "brown",
+        borderWidth: 2,
+    },
+    statsContainer: {
+        borderColor: "blue",
+        borderWidth: 1,
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+        marginBottom: 10,
+    },
+    statsTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 10,
+        marginTop: 40,
+    },
+    iconGroup: {
+        flexDirection: "row",
+        borderColor: "orange",
+        borderWidth: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    Icon: {
+        marginRight: 10,
+    },
+    stats: {
+        borderColor: "red",
+        borderWidth: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "70%",
+        padding: 10,
+        alignItems: "center",
+    },
 });
